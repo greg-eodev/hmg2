bd40b55e12600bd1eee627dcd500bfe3cccde41d
Gregory Houtteman <gregh@schoolzone.com>
2022-03-15 10:00:04 -0400

    HMG: the new Harmonagon with Material UI

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,58 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+const Copyright = () => {
+	return (
+		<Typography variant="body2" color="text.secondary" align="center">
+			{'Copyright © 2016 - '}
+			{new Date().getFullYear()}
+			&nbsp;Radical Data and Media LLC
+			{'.'}
+		</Typography>
+	);
+}
+
+const App = () => {
+	return (
+		<Container maxWidth="sm">
+		<Box sx={{ my: 4 }}>
+			<Typography variant="h4" component="h1" gutterBottom>
+				Harmonagon Redux
+			</Typography>
+			<p>Welcome the the rebirth ... re-emergence ... re-something of<br /><strong><Link color="inherit" href="http://harmonagon.com/">The Harmonagon Project</Link></strong></p>
+			<Copyright />
+		</Box>
+		</Container>
+	);
 }
 
 export default App;
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
+import theme from './theme';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+				<CssBaseline />
+				<App />
+			</ThemeProvider>
+		</Provider>
+	</React.StrictMode>,
+  	document.querySelector('#root')
 );
 
 // If you want your app to work offline and load faster, you can change
