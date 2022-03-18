16a4dc1eedb2a6ad7c510ff7716293dd9b219d29
Gregory Houtteman <gregh@schoolzone.com>
2022-03-18 06:10:01 -0400

    Web: repair useless constructor error

--- web/src/classes/HMG.tsx
+++ web/src/classes/HMG.tsx
@@ -1,8 +1,10 @@
 import Phaser from "phaser";
 
 class HMG extends Phaser.Game {
+	public init: number;
 	constructor(config: Phaser.Types.Core.GameConfig) {
 		super(config);
+		this.init = 1;
 	}
 }
 
